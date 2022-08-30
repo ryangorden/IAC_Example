eda825fdec94ba79ba821ee801634065dbc55d47
ryangorden <63272391+ryangorden@users.noreply.github.com>
2022-08-29 22:41:45 -0500

    add schema validation

--- app_hosting.py
+++ app_hosting.py
@@ -1,13 +1,27 @@
+import sys
 from jinja2 import Environment, FileSystemLoader
+from jsonschema import validate
 from yaml import safe_load
 
 
+
+# I am opening my schema file
+with open('config_data/config_contexts_schemes/dnac9300.local.yml') as file:
+    schema = safe_load(file)
+
 # I am opening my data file
-with open('config_context/devices/dnac9300.local.yml') as file:
-    app = safe_load(file)
+with open('config_data/config_contexts/devices/dnac9300.local.yml') as file:
+    good_instance = safe_load(file)
+
+# if None is returned validate passess
+validate= (validate(good_instance, schema)) # passes
+print(validate)
+
+if validate:
+    sys.exit(1)
 
 # This is combining my data file with the template
 env = Environment(loader=FileSystemLoader('.'))
 template = env.get_template('templates/app_hosting.j2')
-commands = template.render(config_context=app)
+commands = template.render(config_context=good_instance)
 print(commands)
